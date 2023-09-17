6cead42a595a70e0a242a87c3d1992f225f3e150
K-I-Nayeem-dev <naeem8541@gmail.com>
2023-09-17 13:34:36 +0600

    Video 8 Complete

--- src/components/TextNodes.js
+++ src/components/TextNodes.js
@@ -3,10 +3,15 @@ import React, {useState} from 'react'
 function TextNodes(props) {
 
     const [count, setCount] = useState(0);
+
     const [text, setText] = useState('Enter default Value');
 
     const clickToCount = ()=>{
         setCount(count + 1)
+    }
+
+    const Counting = ()=>{
+        setCount(count)
     }
 
     const handelOnClick = ()=>{        
@@ -20,9 +25,13 @@ function TextNodes(props) {
     const clearTextArea = ()=>{
         setText('');
     }
+    // const boldText = ()=>{
+    //    let button = document.querySelectorAll('textarea')[0];
+    //    let bold = text.innerHTML =  button.style.fontWeight = 'bold';
+    //    setText(bold);
+    // }
 
     return (
-        <>
         <div className="container my-3">
             <div className="row d-flex justify-content-center">
                 <div className="col-lg-8 my-3">
@@ -30,18 +39,24 @@ function TextNodes(props) {
                         <h1 className='mb-3'>{props.heading}</h1>
                         <div className='d-flex justify-content-center'>
                             <textarea type="text" onChange={handelOnChange} value={text} rows='8' className="form-control" aria-describedby="emailHelp"/>
-                            <textarea type="text" value={count} rows='8' className="form-control" aria-describedby="emailHelp"/>
+                            <textarea type="text" onChange={Counting} value={count} rows='8' className="form-control"/>
                         </div>
                     </div>
 
                     <button onClick={handelOnClick} className="btn btn-primary my-2">Convert To UpperCase</button>
                     <button onClick={clearTextArea} className="btn btn-primary my-2 ms-2">Clear Text Area</button>
+                    {/* <button onClick={boldText} className="btn btn-primary my-2 ms-2">Bold Text</button> */}
 
                 </div>
             </div>
-        </div>
-
-        <div className="container">
+            <div className="row d-flex justify-content-center">
+                <div className="col-lg-10">
+                    <h1>Your Text Summery</h1>
+                    <p>{text.split(' ').length} Words {text.length} Total Lenght To Read {0.008 * text.split(" ").length} Minute</p>
+                    <h2>Preview</h2>
+                    <p>{text}</p>
+                </div>
+            </div>
             <div className="row d-flex justify-content-center">
                 <div className="col-lg-10">
                     <h1 className='text-center'> {count} </h1>
@@ -49,8 +64,6 @@ function TextNodes(props) {
                 </div>
             </div>
         </div>
-
-        </>
 
     )
     
